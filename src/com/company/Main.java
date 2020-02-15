57411bc41241c2eb0b37dd29c9ab8b60341eca42
Eduardo Brito Sanchez <eebritosa@gmail.com>
2020-02-15 19:38:22 +0000

    Cipher done, waiting for decipher and user passed parameters

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,59 @@
 package com.company;
-import java.util.HashMap;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
         char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
         int alphabetSize= alphabet.length;
-        HashMap<Short, Character> alphabetMap = new HashMap<>();
+        HashMap<Character, Short> alphabetMap = new HashMap<>();
+        String M = new String("ESTE MENSAJE SE AUTODESTRUIRA").replaceAll("\\s+","");
+        String K =  new String("SOL");
+        int kLength = K.length();
+        String C = new String();
 
         for (int i = 0; i < alphabetSize; i++) {
-            alphabetMap.put((short) i, alphabet[i]);
+            alphabetMap.put(alphabet[i], (short) i);
         }
 
+        for (int i = 0; i < M.length(); i++) {
+            C += getKeyByValue(alphabetMap, sum(alphabetMap.get(M.charAt(i)),alphabetMap.get(K.charAt(i % kLength)),alphabetSize));
+        }
+
+
     }
+
+    public static <T, E> T getKeyByValue(HashMap<Character, Short> map, int value) {
+        for (Map.Entry<Character, Short> entry : map.entrySet()) {
+            if (Objects.equals((short) value, entry.getValue())) {
+                return (T) entry.getKey();
+            }
+        }
+        return null;
+    }
+
+    static int sum(int a, int b, int m)
+    {
+        int s = a + b;
+        s = s % m;
+        return s;
+    }
+
+
+    @NotNull
+    public static List<String> splitEqually(@NotNull String text, int size) {
+        // Give the list the right capacity to start with. You could use an array
+        // instead if you wanted.
+        List<String> ret = new ArrayList<String>((text.length() + size - 1) / size);
+
+        for (int start = 0; start < text.length(); start += size) {
+            ret.add(text.substring(start, Math.min(text.length(), start + size)));
+        }
+        return ret;
+    }
+
+
 }
